fbfab7052ec5dce4bfd6a55d9d7b78b2f39bc7be
mirelakulenovic <mirela.kulenovic@live.se>
2019-04-10 20:27:22 +0200

    fixed html, added photo, css, js

--- js/script.js
+++ js/script.js
@@ -5,4 +5,12 @@ $(document).ready(function(){
         play: 5000
     });
 
+    var typed = new Typed(".typed", {
+        strings: ["Software Developer.", "Web Developer."],
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
+    });
+
 });

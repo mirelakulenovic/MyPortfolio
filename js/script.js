d0d36fad828f685314a26a6c6e46eb541b509d5f
mirelakulenovic <mirela.kulenovic@live.se>
2019-04-10 23:22:34 +0200

    css

--- js/script.js
+++ js/script.js
@@ -13,4 +13,38 @@ $(document).ready(function(){
         showCursor: false
     });
 
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        items: 5,
+        responsive:{
+            0:{
+                items:1
+            },
+            480:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            938:{
+                items:4
+            },
+            1024:{
+                items:5
+            }
+        }
+    });
+
+    $('.chart').easyPieChart({
+        easing: 'easeInOut',
+        barColor: '#fff',
+        trackColor: false,
+        scaleColor: false,
+        lineWidth: 4,
+        size: 152,
+        onStep: function(from, to, percent) {
+            $(this.el).find('.percent'). text(Math.round(percent));
+        }
+    });
+
 });
